362cc5dabd5636e3ce7e28f1a427df1bafefb3c3
shubham920816 <shubhaM1674@>
2023-05-21 19:58:25 +0530

    Day 1 Microservices (Base Structure)

--- product-service/src/test/java/com/org/shubham/ProductServiceApplicationTest.java
+++ product-service/src/test/java/com/org/shubham/ProductServiceApplicationTest.java
@@ -1,9 +1,13 @@
 package com.org.shubham;
 
 import com.org.shubham.dtos.ProductRequestDto;
+import com.org.shubham.entities.Product;
+import com.org.shubham.repositories.ProductRepository;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.MockMvcAutoConfiguration;
@@ -33,6 +37,12 @@ public class ProductServiceApplicationTest
     @Autowired
     private MockMvc mockkMvc;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Autowired
+    private ProductRepository productRepository;
+
     @Container
     static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
 
@@ -44,11 +54,7 @@ public class ProductServiceApplicationTest
     @Test
     void shouldCreateProduct() throws Exception {
 
-        ProductRequestDto productRequestDto = new ProductRequestDto();
-        productRequestDto.setProductName("Test Product");
-        productRequestDto.setDescription("Product Service Creation Test");
-        productRequestDto.setProductPrice(35.43);
-
+        ProductRequestDto productRequestDto =  createTestProduct("test","testing",35.03);
         String requestString = new ObjectMapper().writeValueAsString(productRequestDto);
 
         mockkMvc.perform(MockMvcRequestBuilders.post("/api/v1/product")
@@ -56,9 +62,30 @@ public class ProductServiceApplicationTest
                 .content(requestString))
                 .andExpect(status().isCreated());
 
+        Assertions.assertEquals(1, productRepository.findAll().size());
 
+    }
 
+     ProductRequestDto createTestProduct(String productName,
+                                         String description,Double productPrice) throws JsonProcessingException {
 
+         ProductRequestDto productRequestDto = new ProductRequestDto();
+         productRequestDto.setProductName(productName);
+         productRequestDto.setDescription(description);
+         productRequestDto.setProductPrice(productPrice);
+         return productRequestDto;
+     }
+
+    @Test
+    void getProductsTest() throws Exception{
+
+        ProductRequestDto productRequestDto =  createTestProduct("test","testing",35.03);
+        String requestString = new ObjectMapper().writeValueAsString(productRequestDto);
+
+        mockkMvc.perform(MockMvcRequestBuilders.get("/api/v1/product")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(requestString))
+                .andExpect(status().isOk());
     }
 
 }
