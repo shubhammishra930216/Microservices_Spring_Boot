b58d3dbe9e2e64ddd81feb591829b97e6745c412
shubham920816 <shubhaM1674@>
2023-05-21 19:26:43 +0530

    Day 1 Microservices (Base Structure)

---
+++ inventory-service/src/main/java/com/org/shubham/InventoryServiceApplication.java
@@ -0,0 +1,17 @@
+package com.org.shubham;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class InventoryServiceApplication
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(InventoryServiceApplication.class, args);
+    }
+}
